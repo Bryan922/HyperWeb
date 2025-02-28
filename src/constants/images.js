0246a1957f035f924f3b09f4823971cf43c0a048
Bryan922 <your@email.com>
2025-02-28 01:22:19 +0100

    Fix icon imports in images.js

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,11 +1,11 @@
 import tradingAlgo from '../assets/01002826723218-web-tete.jpg';
-import { star, shield, send } from '../assets';
+import { star as Star, shield as Shield, send as Send } from '../assets';
 
 export const images = {
   hero: tradingAlgo,
   icons: {
-    tech: star,
-    risk: shield,
-    trending: send
+    techIcon: Star,
+    riskIcon: Shield,
+    trendingIcon: Send
   }
 }; 
